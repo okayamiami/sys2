de349aae53e212358dcc99f6555d8f8b8cd0a480
daichi2374506 <knz2374506@stu.o-hara.ac.jp>
2024-11-15 11:28:27 +0900

    menu保護者

--- src/bussystem/main/ParentsEditAction.java
+++ src/bussystem/main/ParentsEditAction.java
@@ -1,5 +1,21 @@
 package bussystem.main;
 
-public class ParentsEditAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import tool.Action;
+
+public class ParentsEditAction extends Action {
+	//saveParentsUserInfo
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		// セッションを取得
+		HttpSession session = req.getSession(true);
+		//ログインユーザーの取得
+		String user_type = (String) session.getAttribute("user_type");
+		
+		//
+	}
 }
