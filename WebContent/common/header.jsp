6f5dd92afaf83689fe8e9a06a10450289859081b
touya70 <nagaminetouya@gmail.com>
2024-11-15 11:25:26 +0900

    もう一度ログインヘッダー修正

--- WebContent/common/header.jsp
+++ WebContent/common/header.jsp
@@ -1,23 +1,23 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
 <div>
-    <h1>得点管理システム</h1>
+  <h1>得点管理システム</h1>
 </div>
 
-<c:if test="${user != null}">
-    <div>
-        <c:choose>
-            <!-- ParentsUserの場合 -->
-            <c:when test="${user is bean.ParentsUser}">
-                <span>${user.parents_name}様</span>
-            </c:when>
-            <!-- ManageUserの場合 -->
-            <c:when test="${user is bean.ManageUser}">
-                <span>${user.user_name}様</span>
-            </c:when>
-        </c:choose>
-        <a href="Syslogout.action">ログアウト</a>
-    </div>
+<c:if test="${user.isAuthenticated()}">
+  <div>
+    <span>
+      <c:choose>
+        <c:when test="${user_type == 'M'}">
+          ${user.getUser_name()}先生、ようこそ！
+        </c:when>
+        <c:when test="${user_type == 'P'}">
+          ${user.getParents_name()}様、ようこそ！
+        </c:when>
+      </c:choose>
+    </span>
+    <a href="Syslogout.action">ログアウト</a>
+  </div>
 </c:if>
+
--- src/bussystem/LoginExecuteAction.java
+++ src/bussystem/LoginExecuteAction.java
@@ -18,58 +18,53 @@ public class LoginExecuteAction extends Action{
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		HttpSession session = req.getSession(true);
+	    HttpSession session = req.getSession(true);
+	    String url = "";
+	    Map<String, String> errors = new HashMap<>();
 
-		String url = "";
-		Map<String, String> errors = new HashMap<>();
+	    ManageUser MU = new ManageUser();
+	    ManageUserDao MUDao = new ManageUserDao();
+	    ParentsUser PU = new ParentsUser();
+	    ParentsUserDao PUDao = new ParentsUserDao();
+	    String user_type = null;
 
-		ManageUser MU = new ManageUser();
-		ManageUserDao MUDao = new ManageUserDao();
-		ParentsUser PU = new ParentsUser();
-		ParentsUserDao PUDao = new ParentsUserDao();
-		String user_type = null;
+	    String user_id = req.getParameter("user_id");
+	    String user_pass = req.getParameter("user_pass");
+	    String facility_id = req.getParameter("facility_id");
 
-		String user_id = req.getParameter("user_id");
-		String user_pass = req.getParameter("user_pass");
-		String facility_id = req.getParameter("facility_id");
-
-		if(user_id.contains("M") || user_id.contains("T")){
-			if(user_id.contains("M")){
-				user_type = ("M");
-			} else if (user_id.contains("T")){
-				user_type = ("T");
-			}
-			MU = MUDao.login(user_id, user_pass, facility_id);
-			if(MU == null){
-				errors.put("kome", "ログインに失敗しました。IDまたはパスワードが違います。");
-				req.setAttribute("errors", errors);
-				req.getRequestDispatcher("login.jsp").forward(req, res);
-			} else {
-				MU.setAuthenticated(true);
-				//セッションに"user"という変数名で値はTeacher変数の中身
-				session.setAttribute("user", MU);
-				session.setAttribute("user_type", user_type);
-				url = "main/Menu.action";
-				res.sendRedirect(url);
-			}
-
-
-		} else if(user_id.contains("P")) {
-			user_type = ("P");
-			PU = PUDao.parentsLogin(user_id, user_pass, facility_id);
-			if(PU == null){
-				errors.put("kome", "ログインに失敗しました。IDまたはパスワードが違います。");
-				req.setAttribute("errors", errors);
-				req.getRequestDispatcher("login.jsp").forward(req, res);
-			} else {
-				PU.setAuthenticated(true);
-				session.setAttribute("user", PU);
-				session.setAttribute("user_type", user_type);
-				url = "main/Menu.action";
-				res.sendRedirect(url);
-			}
-		}
+	    if(user_id.contains("M") || user_id.contains("T")){
+	        if(user_id.contains("M")){
+	            user_type = ("M");
+	        } else if (user_id.contains("T")){
+	            user_type = ("T");
+	        }
+	        MU = MUDao.login(user_id, user_pass, facility_id);
+	        if(MU == null){
+	            errors.put("kome", "ログインに失敗しました。IDまたはパスワードが違います。");
+	            req.setAttribute("errors", errors);
+	            req.getRequestDispatcher("login.jsp").forward(req, res);
+	        } else {
+	            MU.setAuthenticated(true);
+	            // セッションに "user" として ManageUser インスタンスを格納
+	            session.setAttribute("user", MU);
+	            session.setAttribute("user_type", user_type);
+	            url = "main/Menu.action";
+	            res.sendRedirect(url);
+	        }
+	    } else if(user_id.contains("P")) {
+	        user_type = ("P");
+	        PU = PUDao.parentsLogin(user_id, user_pass, facility_id);
+	        if(PU == null){
+	            errors.put("kome", "ログインに失敗しました。IDまたはパスワードが違います。");
+	            req.setAttribute("errors", errors);
+	            req.getRequestDispatcher("login.jsp").forward(req, res);
+	        } else {
+	            PU.setAuthenticated(true);
+	            session.setAttribute("user", PU);
+	            session.setAttribute("user_type", user_type);
+	            url = "main/Menu.action";
+	            res.sendRedirect(url);
+	        }
+	    }
 	}
 }
-
-
